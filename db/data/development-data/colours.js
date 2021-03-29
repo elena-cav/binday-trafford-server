2d571876bfbe6a1518b6a43094298979ce78768a
elena-cav <elenacavallero@outlook.com>
2021-03-29 18:24:29 +0100

    add linebreaks

--- db/data/development-data/colours.js
+++ db/data/development-data/colours.js
@@ -2,21 +2,21 @@ module.exports = [
   {
     colour: 'blue',
     recyclableItems:
-      'Paper and newspapers \n \u2022 Magazines and catalogues \n \u2022 Books \n \u2022 Cardboard \n \u2022 Card packaging \n \u2022 Food and drink cartons (Tetrapac) \n \u2022 Greetings cards \n \u2022 Wrapping paper (not foil based) \n \u2022 Envelopes and mail \n \u2022 Junk mail'
+      'Paper and newspapers \r\n \u2022 Magazines and catalogues \r\n \u2022 Books \r\n \u2022 Cardboard \r\n \u2022 Card packaging \r\n \u2022 Food and drink cartons (Tetrapac) \r\n \u2022 Greetings cards \r\n \u2022 Wrapping paper (not foil based) \r\n \u2022 Envelopes and mail \r\n \u2022 Junk mail'
   },
   {
     colour: 'grey',
     recyclableItems:
-      'The grey bin is for waste you cannot recycle including: \n \u2022 Plastic pots, tubs and trays \n \u2022 Other plastic packaging (e.g. empty crisp packets) \n \u2022 Plastic bags \n \u2022 Polystyrene \n \u2022 Cotton wool and make up pads \n \u2022 Nappies and sanitary products \n \u2022 Tissues and wet wipes'
+      'The grey bin is for waste you cannot recycle including: \r\n \u2022 Plastic pots, tubs and trays \r\n \u2022 Other plastic packaging (e.g. empty crisp packets) \r\n \u2022 Plastic bags \r\n \u2022 Polystyrene \r\n \u2022 Cotton wool and make up pads \r\n \u2022 Nappies and sanitary products \r\n \u2022 Tissues and wet wipes'
   },
   {
     colour: 'black',
     recyclableItems:
-      'Please rinse your items before you put them in the bin. \n \u2022  Aerosols (no lids) \n \u2022 Foil and foil trays \n \u2022 Food and drinks cans \n \u2022 Bottle shaped plastics (no lids) such as: \n \u2022 Cleaning product bottles \n \u2022 Trigger spray bottles \n \u2022 Shampoo and shower gel bottles \n \u2022 Drinks bottles \n \u2022 Milk bottles \n \u2022 Glass bottles and jars (no lids)'
+      'Please rinse your items before you put them in the bin. \r\n \u2022  Aerosols (no lids) \r\n \u2022 Foil and foil trays \r\r\n \u2022 Food and drinks cans \r\n \u2022 Bottle shaped plastics (no lids) such as: \r\n \u2022 Cleaning product bottles \r\n \u2022 Trigger spray bottles \r\n \u2022 Shampoo and shower gel bottles \r\n \u2022 Drinks bottles \r\n \u2022 Milk bottles \r\n \u2022 Glass bottles and jars (no lids)'
   },
   {
     colour: 'green',
     recyclableItems:
-      'Raw and cooked meat and fish – including bones \n \u2022 Raw and cooked fruit and vegetables \n \u2022 Leftovers from your plate \n \u2022 Bread, cake and pastries \n \u2022 Cheese and eggs – including egg shells \n \u2022 Tea bags and coffee grounds \n \u2022 What garden waste can be recycled? \n \u2022 Flowers and plants [1] \n \u2022 Leaves from the garden \n \u2022 Grass cuttings and hedge clippings \n \u2022 Twigs and small branches \n \u2022 Hay and straw pet bedding (e.g. rabbits and guinea pigs)'
+      'Raw and cooked meat and fish – including bones \r\n \u2022 Raw and cooked fruit and vegetables \r\n \u2022 Leftovers from your plate \r\n \u2022 Bread, cake and pastries \r\n \u2022 Cheese and eggs – including egg shells \r\n \u2022 Tea bags and coffee grounds \r\n \u2022 What garden waste can be recycled? \r\n \u2022 Flowers and plants [1] \r\n \u2022 Leaves from the garden \r\n \u2022 Grass cuttings and hedge clippings \r\n \u2022 Twigs and small branches \r\n \u2022 Hay and straw pet bedding (e.g. rabbits and guinea pigs)'
   }
 ];
